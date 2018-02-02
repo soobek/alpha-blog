263874e097a40c0d4f946d24ffed10bb365faee5
soobek <soobek@o2.pl>
2018-02-02 23:24:13 +0000

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  get 'pages/home', to: 'pages#home'
+  root 'pages#home'
   get 'pages/about', to: 'pages#about'
 
   # Example of regular route:
