3446a80b9a85081dc75fd5786f381afff4542601
soobek <soobek@o2.pl>
2018-02-17 13:21:17 +0000

    Signup users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
             uniqueness: {case_sensitive: false},
             length: { maximum: 125 },
             format: { with: VALID_EMAIL_REGEX }
+            
   has_secure_password
 end
